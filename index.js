083fcc4f4b7dfeef40a6584f51b58a652fac2eb1
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-16 06:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 class BoardMember {
-  Constructor(name, homeState, training){
+  Constructor( name, homeState, training){
     this.name = name
     this.homeState = homeState
     this.training = training
